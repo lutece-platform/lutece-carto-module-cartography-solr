4063b92c4d55ff0e0f224a703fe7f1c0a3167268
jlf-eviden <jlf-eviden@DESKTOP-BCRT9V4>
2025-11-27 16:40:35 +0100

    LUT-31310 : Adding liquibase tags to SQL scripts

--- src/sql/plugins/cartosolr/core/init_core_carto.sql
+++ src/sql/plugins/cartosolr/core/init_core_carto.sql
@@ -1,3 +1,6 @@
+-- liquibase formatted sql
+-- changeset cartography-solr:init_core_carto.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 DELETE FROM core_admin_right WHERE id_right = 'CARTO_EXPORT_SOLR';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
 ('CARTO_EXPORT_SOLR','module.cartography.solr.export.name',1,'jsp/admin/plugins/cartosolr/ManageExportDataLayer.jsp','module.cartography.solr.export.description',0,'carto',NULL,NULL,NULL,4);
